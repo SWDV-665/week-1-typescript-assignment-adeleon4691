e5fe2f56e29be9aeb7dbc92429223e40f2ac98b8
Andrew DeLeon <67667253+adeleon4691@users.noreply.github.com>
2021-10-25 00:51:48 -0400

    Add files via upload

---
+++ grocery.ts
@@ -0,0 +1,16 @@
+const grocery = {
+    name: 'Lettuce',
+    total: 1
+};
+
+const grocery2 = {
+    name: 'Apples',
+    total: 4
+};
+
+const grocery3 = {
+    name: 'Bananas',
+    total: 6
+};
+
+
